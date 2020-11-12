4df2da1b8cfad62379791a2fb2acb599d907e241
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2020-11-12 17:01:50 +0700

    Create Variable.c

---
+++ Bài tập 1/Variable.c
@@ -0,0 +1,26 @@
+int main(void) {
+  //Khai bao bien a kieu so nguyen
+  int a;
+  //Khoi tao gia tri bien a
+  a = 4;
+  //Khai bao va khoi tao bien
+  int b = 2;
+  //Khai bao nhieu bien va khoi tao
+  int c = 1, d = 3; 
+  //Khai bao nhieu bien 
+  int e, f;	
+  printf ("Bien a = %d va bien b = %d ",a,b);
+  e = a + b;
+  printf ("\n");
+  printf ("Tong a + b = %d",e);
+  printf ("\n");
+  printf ("Tong %d + %d = %d",a,b,e);
+  f =(( a + b ) - c)*d;
+  printf ("\n");
+  printf ("\n"); 
+ 
+
+  getchar();
+  return 0;
+  
+}
