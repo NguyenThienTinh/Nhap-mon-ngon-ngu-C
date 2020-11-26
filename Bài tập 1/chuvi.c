f024d015e55ce16411aa891c281b565078c05b43
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2020-11-26 16:46:37 +0700

    Update chuvi.c

--- Bài tập 1/chuvi.c
+++ Bài tập 1/chuvi.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
 int main (void){
-	//khai bao R kieu double va gan R=2.25
+	//khai bao bien R kieu double 
 	double R;
-		R = 2.5;
+	R = 2.5;
 		
-	//khai bao PI=3.14 kieu double	
+	//Khai bao PI kieu double
 	double PI =3.14;
 	
-	//in chu vi va dien tich ra man hinh 
+	//Chu vi va dien tich  
 	printf("Chu vi\t   Dien Tich\n");
 	
-	//khai bao VC va DT
+	//khai bao CV va DT
 	double CV , DT;
 	
-	//gan va ep kieu double VC voi cong thuc 2*R*PI va DT voi cong thuc la R*R*PI 
+	//Gan va ep kieu double CV  
 	CV =(double) 2*R*PI;
 	DT =(double) R*R*PI;
 	
-	//in ket qua CV va DT ra man hinh
+	//Xuat ket qua 
 	printf("%g\t   %g",CV,DT);
+	
 	
 	return 0;
 }
