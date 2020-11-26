2d5bbd1e99c68628960de7333a541682dfc4014f
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2020-11-26 13:38:08 +0700

    Create pheptinh.c

---
+++ Bài tập 1/pheptinh.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+	
+  int a; int b;
+  
+  a= 3; b= 5;
+  
+  int tong; int hieu; int tich; int thuong;
+  
+  tong= a+b; hieu= a-b; thuong= a/b; tich= a*b;
+  
+  printf("tong= %d\n",a+b);
+
+  printf("hieu= %d\n",a-b);
+ 
+  printf("tich= %d\n",a*b);
+  
+  printf("thuong= %d\n",a/b);
+  double c = 3;
+  double d = 5;
+  printf("thuong = %f", c/d);
+  
+  
+  getchar ();
+  return 0;
+}
