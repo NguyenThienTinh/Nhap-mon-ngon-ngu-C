1c5851bb35f40d01b290bfa519d02bb0e36d6f49
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2020-11-26 14:51:31 +0700

    Add files via upload

--- Bài tập 1/pheptinh.c
+++ Bài tập 1/pheptinh.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
-int main(void) {
+int main (void){
 	
-  int a; int b;
-  
-  a= 3; b= 5;
-  
-  int tong; int hieu; int tich; int thuong;
-  
-  tong= a+b; hieu= a-b; thuong= a/b; tich= a*b;
-  
-  printf("tong= %d\n",a+b);
-
-  printf("hieu= %d\n",a-b);
- 
-  printf("tich= %d\n",a*b);
-  
-  printf("thuong= %d\n",a/b);
-  double c = 3;
-  double d = 5;
-  printf("thuong = %f", c/d);
-  
-  
-  getchar ();
-  return 0;
+	int a ;int b;
+	a=3;b=5;
+	
+	printf("a\t"); printf("b\t"); printf("a+b\t"); printf("a-b\t"); printf("a*b\t"); printf("a/b\n");
+	
+	
+	a+b; a-b; a*b; a/b;
+	
+	printf("%d\t",a); printf("%d\t",b);
+	 
+	printf("%d\t",a+b); printf("%d\t",a-b);
+	
+	printf("%d\t",a*b); 
+	
+	double c = 3;
+    double d = 5;
+    printf("%g", c/d); 
+	 
+	return 0;
 }
-
