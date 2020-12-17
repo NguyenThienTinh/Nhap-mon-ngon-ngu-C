b043a39f5143e082fb10cad4b10b2dcf141717d1
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2020-12-17 16:07:39 +0700

    Add files via upload

---
+++ Bài tập 3/pheptinh.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	//Khai bao bien a,b
+  int a = 6, b = 3;
+  	// Kieu ki tu 
+  char pt;
+  printf ("vui long moi ban nhap phep tinh vao may tinh ");
+  scanf ("%c",&pt);
+  switch(pt) {
+  	// Phep tinh +
+    case '+': 
+	 		printf (" a + b = %d",a + b);
+      break;
+    // Phep tinh - 
+  	case '-':
+	   		printf (" a - b = %d",a - b);
+      break;
+      default:
+       printf(" Moi ban nhap phep + hoac - ");
+  }
+  //Xuat ket qua 
+  return 0;
+}
