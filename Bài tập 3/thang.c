1cec3d757f5f7d915f073720087e94df9c9a898f
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2020-12-17 16:37:48 +0700

    Add files via upload

---
+++ Bài tập 3/thang.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main(){
+	//Khai bao bien
+  char pt;
+  int thang;
+  printf ("vui long moi ban nhap thang vao may tinh ");
+  scanf ("%d",&thang);
+  switch(thang) 
+  {
+    case 2: 
+	 	printf ("28 ngay");
+      break;
+    case 3: 
+	 	printf ("31 ngay"); 
+	  break;
+	case 4: 
+	 	printf ("30 ngay");
+      break;
+	case 5: 
+	 	printf ("31 ngay");
+      break;
+	case 6: 
+	 	printf ("30 ngay");
+      break;
+	case 7: 
+	 	printf ("31 ngay");
+      break;
+	case 8: 
+	 	printf ("31 ngay");
+      break;
+	case 9: 
+	 	printf ("30 ngay");
+      break;
+	case 10: 
+	 	printf ("31 ngay");
+      break;
+	case 11: 
+	 	printf ("30 ngay");
+      break;
+	case 12: 
+	 	printf ("31 ngay");
+      break;
+	          	  		 
+  	}
+  //Xuat ket qua 
+  return 0;
+}
