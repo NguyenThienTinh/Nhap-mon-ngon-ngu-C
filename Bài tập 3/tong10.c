cf3d3fbd59c0d39208b95290e882b636930f13f3
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2020-12-24 14:35:00 +0700

    Create tong10.c

---
+++ Bài tập 3/tong10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+//Khai bao bien sum
+ int sum = 0;
+ int count;
+ //Hien thi tong tu 1 - 10
+ for( count = 1; count <= 10; count++){
+   sum = sum + count;
+   }
+  printf("tong tu 1 den 10 = %d", sum);
+//Xuat ket qua 
+  return 0;
+  }
