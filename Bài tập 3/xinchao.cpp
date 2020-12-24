1dd8da29e7b6f8183059068229a065fe6381202a
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2020-12-24 13:50:20 +0700

    Add files via upload

---
+++ Bài tập 3/xinchao.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Khai bao bien
+int main(void) {
+// Khai bao so dem
+  int count;
+// In ra 10 dong xin chao
+  for(count = 1; count <= 10; count++){
+    printf("\n xinchao");
+    }
+  
+// Xuat ket qua 
+  return 0;
+}
