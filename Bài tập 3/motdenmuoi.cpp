94d038f05ae4a0dff9486b8506dbc39f5d1d8b7d
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2020-12-24 14:14:00 +0700

    Add files via upload

---
+++ Bài tập 3/motdenmuoi.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//Khai bao bien
+int main(void) {
+//Khai bao so dem tu 1 - 10 
+  for(int i = 1; i < 11; i++){
+    printf(" %d", i);
+    }
+//Hien thi tu 1 - 10 tren 10 hang
+  int count;
+  for(count = 1 ; count < 11; count++){
+    printf("\n 1 2 3 4 5 6 7 8 9 10  ");  
+  }
+//Xuat ket qua
+  return 0;
+}
+
