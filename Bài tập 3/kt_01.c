4ffce2498e1e0c96deb7373f70a66579ebad7700
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2021-01-07 16:29:15 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -2,12 +2,14 @@ int main(){
 	//Khai bao cac bien
 	int a, b, c,d;
 	a=1,b=2,c=3,d=4;
-	//Cac bang cuu chuong tu , tu cuu chuong 2 den cuu chuong 4
-	printf("ban cuu chuong 2\n");
+	//Cac bang cuu chuong , bang cuu chuong 2
+	printf("Ban Cuu Chuong 2\n");
 	printf("2x1=%d\n2x2=%d\n2x3=%d\n",b,d,b+d);
-	printf("bang cuu chuong 3\n");
+	//bang cuu chuong 3
+	printf("Bang Cuu Chuong 3\n");
 	printf("3x1=%d\n3x2=%d\n3x3=%d\n",c,d+b,c*c);
-	printf("bang cuu chuong 4\n");
+	//bang cuu chuong 4
+	printf("Bang Cuu Chuong 4\n");
 	printf("4x1=%d\n4x2=%d\n4x3=%d\n",d,b*d,c*d);
 	
 	//Xuat ket qua
