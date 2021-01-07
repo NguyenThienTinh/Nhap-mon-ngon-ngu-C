be611a78dc64720b04404fb0dedda3884ce76ecf
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2021-01-07 16:35:34 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -4,13 +4,13 @@ int main(){
 	a=1,b=2,c=3,d=4;
 	//Cac bang cuu chuong , bang cuu chuong 2
 	printf("Ban Cuu Chuong 2\n");
-	printf("2x1=%d\n2x2=%d\n2x3=%d\n",b,d,b+d);
+	printf("2x1=%d\n 2x2=%d\n 2x3=%d\n",b,d,b+d);
 	//bang cuu chuong 3
 	printf("Bang Cuu Chuong 3\n");
-	printf("3x1=%d\n3x2=%d\n3x3=%d\n",c,d+b,c*c);
+	printf("3x1=%d\n 3x2=%d\n 3x3=%d\n",c,d+b,c*c);
 	//bang cuu chuong 4
 	printf("Bang Cuu Chuong 4\n");
-	printf("4x1=%d\n4x2=%d\n4x3=%d\n",d,b*d,c*d);
+	printf("4x1=%d\n 4x2=%d\n 4x3=%d\n",d,b*d,c*d);
 	
 	//Xuat ket qua
 	return 0;
