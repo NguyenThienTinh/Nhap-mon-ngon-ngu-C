733dfa5b9a60d524de50ce19df0e8ae8c799c52f
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2021-01-08 07:52:25 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -6,7 +6,7 @@ int main(void) {
   printf("Nhap Bang Cuu Chuong Ma Ban Muon");
   scanf("%d", &n);
    for(int i = 1; i <= n; i++){
-     printf("BẢNG NHÂN %d\n", i);
+     printf("Bảng Nhân %d\n", i);
     for (int j = 0; j <= 4; j++){
       printf("\t%d * %d = %d\n", i, j, i*j);
       } 
