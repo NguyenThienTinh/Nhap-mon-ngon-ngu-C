df8ef5e9934af74958706880c00753fc1f198dc4
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2021-01-08 07:49:27 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,17 +1,16 @@
-int main(){
-	//Khai bao cac bien
-	int a, b, c,d;
-	a=1,b=2,c=3,d=4;
-	//Cac bang cuu chuong , bang cuu chuong 2
-	printf("Ban Cuu Chuong 2\n");
-	printf("2x1=%d\n 2x2=%d\n 2x3=%d\n",b,d,b+d);
-	//bang cuu chuong 3
-	printf("Bang Cuu Chuong 3\n");
-	printf("3x1=%d\n 3x2=%d\n 3x3=%d\n",c,d+b,c*c);
-	//bang cuu chuong 4
-	printf("Bang Cuu Chuong 4\n");
-	printf("4x1=%d\n 4x2=%d\n 4x3=%d\n",d,b*d,c*d);
-	
-	//Xuat ket qua
-	return 0;
+#include <stdio.h>
+
+int main(void) {
+//KHAI BAO BIEN
+  int n;
+  printf("Nhap Bang Cuu Chuong Ma Ban Muon");
+  scanf("%d", &n);
+   for(int i = 1; i <= n; i++){
+     printf("BẢNG NHÂN %d\n", i);
+    for (int j = 0; j <= 4; j++){
+      printf("\t%d * %d = %d\n", i, j, i*j);
+      } 
+    }  
+ //XUAT KET QUA 	
+  return 0;
 }
