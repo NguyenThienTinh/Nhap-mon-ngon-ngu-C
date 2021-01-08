3ab21bc16640ae30f4130e04916884768ae15e61
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2021-01-08 09:13:57 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -4,17 +4,17 @@ int main(){
 int n,a,S=0;
 
 	printf("Nhap so n =  "); scanf("%d",&n);
-    
+    // chia lay so du VD: 123 / 10 =12.3 ( được 12 dư 3)
 	a = n % 10; 
-	S+= a ;
+	S= S + a ;
 	n/= 10;
 	
-	a = n%10; 
-	S += a;
-	n /= 10;
+	a = n % 10; 
+	S= S + a ;
+	n/= 10;
 	
-	a = n%10; 
-	S += a;
+	a = n % 10; 
+	S= S + a ;
 	n/= 10;
 		
 	printf("%d\n",S);
