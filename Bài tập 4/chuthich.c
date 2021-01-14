32d6152a59429464ebc7f9f20dd103826a407c13
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2021-01-14 15:21:57 +0700

    Create chuthich.c

---
+++ Bài tập 4/chuthich.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main() {
+int a = 3;// Khai bao 1 ngan
+int b = [3]// Khai bao 3 ngan
+b[0] = 3;// Ngan thu 1
+b[1] = 4;// Ngan thu 2
+b[2] = 1;// Ngan thu 3
+
+float c[4] = { 0,1,2,3} khai bao cac phan tu
