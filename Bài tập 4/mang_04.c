6c1cccfb471cc11809d5b1ab6fe98a9368c9c2f2
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2021-01-19 16:31:48 +0700

    Update mang_04.c

--- Bài tập 4/mang_04.c
+++ Bài tập 4/mang_04.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+ 
+int main()
+{
+   //Khai bao bien
+    int	n,s=0;
+    printf("Nhap so nguyen n: ");
+    scanf("%d", &n);
+      int a[n];
 
+    for(int i = 0; i < n; i++){
+      printf("Nhap phan tu n %d: ", i + 1 );
+      scanf("%d", &a[i]);
+    }
+        printf("Cac phan tu trong mang a: ");
+    for(int i = 0; i < n; i++){
+      printf(" %d",a[i]);
+       s = s + a[i];
+    }
+        printf("\nTong cac con so trong mang = %d",s);
+     return 0;
+}
