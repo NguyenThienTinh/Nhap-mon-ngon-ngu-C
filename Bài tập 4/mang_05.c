502cdac22d7e503ee41102afd65c43688a9c48b1
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2021-01-21 16:17:13 +0700

    Update mang_05.c

--- Bài tập 4/mang_05.c
+++ Bài tập 4/mang_05.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+ 
+int main(){
 
+int n,m;
+printf("Nhap so nguyen n: ");
+scanf("%d", &n);
+
+int a[n];
+for (int i = 0; i < n; i++) 
+{
+printf("\nNhap phan tu thu %d: ", i + 1);
+scanf("%d", &a[i]);
+}
+
+printf("\nNhap mang a: ");
+for (int i = 0; i < n; i++)
+printf(" %d", a[i]);
+
+int b = 0;
+printf("\nNhap so nguyen m ma ban muon: ");
+scanf("%d", &m);
+for(int i = 0; i < n; i++){
+ if (a[i]==m) {
+   b = b + 1;
+ }
+}
+if (b > 0){
+  printf("Co ton tai ");
+}
+else {
+  printf("Khong ton tai");
+}
+
+  return 0;
+}
