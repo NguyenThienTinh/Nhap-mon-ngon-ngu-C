88110ccd454a5599e64c74fcf8b382280c409852
NguyenThienTinh <72543309+NguyenThienTinh@users.noreply.github.com>
2021-01-22 09:30:55 +0700

    Create ham_02.c

---
+++ Bài tập 5/ham_02.c
@@ -0,0 +1,18 @@
+//Khoi khai bao
+#include<stdio.h>
+
+ //khoi ham main
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  if(kiem_tra_am_duong(n) > 0)
+      printf("n la so nguyen duong ");
+  else if (kiem_tra_am_duong(n) < 0)
+    printf("n la so nguyen âm");
+  else 
+    printf("n = 0");
+  return 0;
+}
+
+// khoi dinh nghia ham
